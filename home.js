b090b4f59be978bb2820e53da354c5e3b2a9e000
Sammi Turner <ms.sammi.turner@gmail.com>
2020-06-05 15:24:50 +0100

    Add files via upload

---
+++ home.js
@@ -0,0 +1,33 @@
+var homeString = `
+<style>
+  a {
+    margin: 0.5rem;
+    color: #282a36;
+    font-size: 2rem;
+  }
+</style>
+
+<div>
+  <h1 class="title">Norman Cat</h1>
+  <h3 class="subtitle">Kitteh Developer</h3>
+  <br />
+  <a href="https://github.com/sammi-turner/Vanilla-JavaScript-Personal-Site-Template">
+    <i class="fab fa-github"></i>
+  </a>
+  <a href="https://github.com/sammi-turner/Vanilla-JavaScript-Personal-Site-Template">
+    <i class="fab fa-linkedin"></i>
+  </a>
+  <p class="subtitle">
+    "Don't just sit on
+    <br />
+    that warm keyboard.
+    <br />
+    Type on it as well.
+    <br />
+    Never pause, with
+    <br />
+    your paws."
+    <br /><br />
+  </p>
+</div>
+`
