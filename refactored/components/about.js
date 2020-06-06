08c581bbf5ff59da6d19a477df2fee2e43ec9a15
Sammi Turner <ms.sammi.turner@gmail.com>
2020-06-06 18:57:56 +0100

    Add files via upload

---
+++ refactored/components/about.js
@@ -0,0 +1,21 @@
+var aboutString = `
+<style>
+  img {
+    margin-top: 10vh;
+    padding: 1rem;
+  }
+</style>
+
+<div>
+  <img src="assets/leet.kitteh.jpg" alt="Norman" />
+    <p class="subtitle">
+      Hello, I'm Norman.
+      <br /><br />
+      I am a cat who
+      <br />
+      is very passionate
+      <br />
+      about front-end.
+    </p>
+</div>
+`
---
+++ refactored/scripts/app.js
@@ -0,0 +1,15 @@
+function homeState() {
+  document.getElementById("pageContent").innerHTML = homeString
+}
+
+function aboutState() {
+  document.getElementById("pageContent").innerHTML = aboutString
+}
+
+function blogState() {
+  document.getElementById("pageContent").innerHTML = blogString
+}
+
+function contactState() {
+  document.getElementById("pageContent").innerHTML = contactString
+}
